4c093f8af7b4e2621db5b671fa3ab42def6f7e08
{Bright-ship-it} <{wilbright.amugune@student.moringaschool.com}>
2022-06-05 02:26:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+function writeCards(listFriends, surprise){
+    let surpriseCard = [];
+    for (let i=0; i<listFriends.length;i++){
+      surpriseCard.push(`Thank you, ${listFriends[i]}, for the wonderful ${surprise} gift!`);
+    }
+
+    return surpriseCard;
+}
+
+writeCards(["Guadalupe", "Olli", "Aki"], "surprise");
+
+
+function countDown(number) {
+  while (number >= 0) {
+      console.log(number);
+      number--;
+  }
+}
+
+countDown(number);
